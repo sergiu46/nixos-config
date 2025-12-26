8a17d745cd70ef9073beb406da01ca1f20924f3e
Sergiu <sergiu46@gmail.com>
2025-12-26 13:55:50 +0200

    fix problems

--- users/denisa/home.nix
+++ users/denisa/home.nix
@@ -1,11 +1,7 @@
-{ pkgs, ... }:
+{ ... }:
 {
   home.username = "denisa";
   home.homeDirectory = "/home/denisa";
-
-  home.packages = with pkgs; [
-
-  ];
 
   programs.bash.enable = true;
   home.stateVersion = "25.11";
