43c731ccf2d8d097ed616f9574d5830b00a4cab4
Sergiu <sergiu46@gmail.com>
2025-12-27 13:57:26 +0200

    vscode

--- users/sergiu/vscode/vscode.nix
+++ users/sergiu/vscode/vscode.nix
@@ -7,18 +7,24 @@ in
   programs.vscode = {
     enable = true;
     package = pkgs.unstable.vscode;
+    
+    # This allows VS Code to manage extensions that aren't declared in Nix
     mutableExtensionsDir = true;
 
     profiles.default = {
+      # This ensures the extension is installed automatically
       extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
       ];
+      
+      # We can also move settings here, but since you want them writable,
+      # we keep the symlink below instead of 'userSettings'.
     };
   };
 
-  # This tells Nix to point the VS Code config to a real file in your repo folder
+  # Writable symlink to your repo
   xdg.configFile."Code/User/settings.json".source =
-    config.lib.file.mkOutOfStoreSymlink "/home/sergiu/NixOS/users/sergiu/vscode/vscode-settings.json";
+    config.lib.file.mkOutOfStoreSymlink "/home/${user}/NixOS/users/${user}/vscode/vscode-settings.json";
 
   home.packages = with pkgs; [
     nixd
