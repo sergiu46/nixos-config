e6e5d0a58efc0bbe9776006ded9f400b34d89596
Sergiu <sergiu46@gmail.com>
2025-12-27 14:54:35 +0200

    Fix problems

--- users/sergiu/vscode/vscode.nix
+++ users/sergiu/vscode/vscode.nix
@@ -7,18 +7,12 @@ in
   programs.vscode = {
     enable = true;
     package = pkgs.unstable.vscode;
-    
-    # This allows VS Code to manage extensions that aren't declared in Nix
     mutableExtensionsDir = true;
 
     profiles.default = {
-      # This ensures the extension is installed automatically
       extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
       ];
-      
-      # We can also move settings here, but since you want them writable,
-      # we keep the symlink below instead of 'userSettings'.
     };
   };
 
@@ -27,8 +21,7 @@ in
     config.lib.file.mkOutOfStoreSymlink "/home/${user}/NixOS/users/${user}/vscode/vscode-settings.json";
 
   home.packages = with pkgs; [
-    nixd
-    nixfmt-rfc-style
-    alejandra
+    nixd # The actual server
+    nixfmt-rfc-style # Formatter
   ];
 }
