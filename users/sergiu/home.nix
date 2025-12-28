85cb484d822df7c6fe21dfaf0eec8ce60a9f886f
Sergiu <sergiu46@gmail.com>
2025-12-28 20:51:31 +0200

    new bash commands

--- users/sergiu/home.nix
+++ users/sergiu/home.nix
@@ -18,19 +18,26 @@
   ];
 
   home.shellAliases = {
-    switch = "sudo nixos-rebuild switch --flake ~/NixOS#Latitude-NIX";
-    check = "nixos-rebuild build --flake ~/NixOS#Latitude-NIX";
-    upswitch = "sudo nix flake update ~/NixOS && sudo nixos-rebuild switch --flake ~/NixOS#Latitude-NIX";
-    mount-portable = "sudo sudo mount /dev/disk/by-label/NIXROOT /mnt && sudo mount /dev/disk/by-label/NIXBOOT /mnt/boot";
-    install-portable = "sudo nixos-install --flake .#Portable-NIX";
+    switch-latitude = "sudo nixos-rebuild switch --flake ~/NixOS#Latitude-NIX";
+    check-latitude = "nixos-rebuild build --flake ~/NixOS#Latitude-NIX";
+    boot-latitude = "sudo nixos-rebuild boot --flake ~/NixOS#Latitude-NIX";
+    update-latitude = "sudo nix flake update ~/NixOS && sudo nixos-rebuild switch --flake ~/NixOS#Latitude-NIX";
+
+    switch-portable = "sudo nixos-rebuild switch --flake ~/NixOS#Portable-NIX";
+    check-portable = "nixos-rebuild build --flake ~/NixOS#Portable-NIX";
+    boot-portable = "sudo nixos-rebuild boot --flake ~/NixOS#Portable-NIX";
+    update-portable = "sudo nix flake update ~/NixOS && sudo nixos-rebuild switch --flake ~/NixOS#Portable-NIX";
+    mount-portable = "sudo mount /dev/disk/by-label/NIXROOT /mnt && sudo mkdir /mnt/boot && sudo mount /dev/disk/by-label/NIXBOOT /mnt/boot";
+    install-portable = "sudo nixos-install --flake ~/NixOS#Portable-NIX";
   };
 
-  # home-manager.users.sergiu = {
-  #   dconf = {
-  #     enable = true;
-  #     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-  #   };
-  # };
+  dconf = {
+    enable = true;
+
+    settings."org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
 
   programs.bash.enable = true;
 
