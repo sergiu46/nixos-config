7d03ea855fd57e9e05b0128d2284aa11fe1d6ab3
Sergiu <sergiu@example.com>
2025-12-29 01:38:16 +0200

    fix commnds

--- users/sergiu/home.nix
+++ users/sergiu/home.nix
@@ -30,12 +30,12 @@
     switch-latitude = "sudo nixos-rebuild switch --flake ~/NixOS#Latitude-NIX";
     check-latitude = "nixos-rebuild build --flake ~/NixOS#Latitude-NIX";
     boot-latitude = "sudo nixos-rebuild boot --flake ~/NixOS#Latitude-NIX";
-    update-latitude = "sudo nix flake update ~/NixOS && sudo nixos-rebuild switch --flake ~/NixOS#Latitude-NIX";
+    update-latitude = "cd ~/NixOS && sudo nix flake update && sudo nixos-rebuild switch --flake ~/NixOS#Latitude-NIX";
 
     switch-portable = "sudo nixos-rebuild switch --flake ~/NixOS#Portable-NIX";
     check-portable = "nixos-rebuild build --flake ~/NixOS#Portable-NIX";
     boot-portable = "sudo nixos-rebuild boot --flake ~/NixOS#Portable-NIX";
-    update-portable = "sudo nix flake update ~/NixOS && sudo nixos-rebuild switch --flake ~/NixOS#Portable-NIX";
+    update-portable = "cd ~/NixOS && sudo nix flake update && sudo nixos-rebuild switch --flake ~/NixOS#Portable-NIX";
     mount-portable = "sudo mount /dev/disk/by-label/NIXROOT /mnt && sudo mkdir /mnt/boot && sudo mount /dev/disk/by-label/NIXBOOT /mnt/boot";
     install-portable = "sudo nixos-install --flake ~/NixOS#Portable-NIX";
   };
