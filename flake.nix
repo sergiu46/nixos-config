f3d869d2095543a84d96bea5da1fa6dcee7d5cf7
Sergiu <sergiu46@gmail.com>
2026-01-02 16:38:33 +0200

    formatting

--- flake.nix
+++ flake.nix
@@ -6,9 +6,7 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    nix-flatpak.url = "github:gmodena/nix-flatpak"; # Stable branch (or pin ?ref=<tag> if needed)
-
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
@@ -27,7 +25,6 @@
       # Common modules shared across all hosts
       commonModules = [
         nix-flatpak.nixosModules.nix-flatpak
-
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
@@ -41,10 +38,8 @@
         Latitude-NIX = nixpkgs.lib.nixosSystem {
           inherit system;
           specialArgs = { inherit inputs stateVersion; };
-
           modules = commonModules ++ [
             ./hosts/Latitude-NIX/configuration.nix
-
             ./users/sergiu/sergiu.nix
             ./users/denisa/denisa.nix
           ];
@@ -53,10 +48,8 @@
         Portable-NIX = nixpkgs.lib.nixosSystem {
           inherit system;
           specialArgs = { inherit inputs stateVersion; };
-
           modules = commonModules ++ [
             ./hosts/Portable-NIX/configuration.nix
-
             ./users/sergiu/sergiu.nix
           ];
         };
