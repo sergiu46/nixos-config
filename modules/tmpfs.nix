9264604303765e54c4de0ed1163b7c400d6c0f49
Sergiu <sergiu@example.com>
2026-01-09 13:34:22 +0200

    Remove comments

--- modules/tmpfs.nix
+++ modules/tmpfs.nix
@@ -10,54 +10,6 @@
   services.journald.extraConfig = "Storage=volatile";
   services.psd.enable = true;
 
-  # systemd.tmpfiles.rules = [
-  #   # Create main dirs
-  #   "d /home/sergiu 0700 sergiu users - -"
-  #   "d /home/sergiu/.var 0700 sergiu users - -"
-
-  #   # Fix permissions
-  #   "Z /home/sergiu/.var    0700 sergiu users  -    -"
-
-  #   # Edge
-  #   "d /tmp/edge-cache 0700 sergiu users - -"
-  #   "L+ /home/sergiu/.var/app/com.microsoft.Edge/cache - - - - /tmp/edge-cache"
-
-  #   # Jellyfin
-  #   "d /tmp/jellyfin-cache 0700 sergiu users - -"
-  #   "L+ /home/sergiu/.var/app/com.github.iwalton3.jellyfin-media-player/cache - - - - /tmp/jellyfin-cache"
-
-  #   # Telegram Cache
-  #   "d /tmp/telegram-cache 0700 sergiu users - -"
-  #   "L+ /home/sergiu/.var/app/org.telegram.desktop/cache - - - - /tmp/telegram-cache"
-
-  #   # Telegram Media Cache (The largest folder)
-  #   "d /tmp/telegram-media-cache 0700 sergiu users - -"
-  #   "L+ /home/sergiu/.var/app/org.telegram.desktop/data/TelegramDesktop/tdata/user_data/media_cache - - - - /tmp/telegram-media-cache"
-
-  # ];
-
-  # system.activationScripts.flatpakPermissions = {
-  #   text = ''
-  #     # Define a helper to reduce repetition
-  #     # Usage: grant <AppID> <Path>
-  #     grant() {
-  #       ${pkgs.flatpak}/bin/flatpak override --user "$1" --filesystem="$2"
-  #     }
-
-  #     echo "Applying Flatpak tmpfs overrides..."
-
-  #     # Edge
-  #     grant com.microsoft.Edge /tmp/edge-cache
-
-  #     # Jellyfin
-  #     grant com.github.iwalton3.jellyfin-media-player /tmp/jellyfin-cache
-
-  #     # Telegram (Needs both paths)
-  #     grant org.telegram.desktop /tmp/telegram-cache
-  #     grant org.telegram.desktop /tmp/telegram-media-cache
-  #   '';
-  # };
-
   fileSystems = {
     "/home/sergiu/.cache" = {
       device = "tmpfs";
