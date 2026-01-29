0003d3da02948e996577ce2dcf82cec01d2d3266
sergiu46 <38151078+sergiu46@users.noreply.github.com>
2026-01-29 07:12:12 +0200

    Create btrfs.nix

---
+++ hosts/Latitude-NIX/btrfs.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+
+{
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "btrfs";
+    options = [ "subvol=@" "compress=zstd:1" "noatime" ];
+  };
+
+  fileSystems."/home" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "btrfs";
+    options = [ "subvol=@home" "compress=zstd:1" "noatime" ];
+  };
+
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "btrfs";
+    options = [ "subvol=@nix" "compress=zstd:1" "noatime" ];
+  };
+
+  fileSystems."/var/log" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "btrfs";
+    options = [ "subvol=@log" "compress=zstd:1" "noatime" ];
+  };
+
+  fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/4804-E951";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+  };
+
+
+}
