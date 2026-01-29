bcc86023e8066d47c02015d94275122c8bd5ef7f
sergiu46 <38151078+sergiu46@users.noreply.github.com>
2026-01-29 07:28:57 +0200

    Sorted apps

--- modules/packages.nix
+++ modules/packages.nix
@@ -3,11 +3,6 @@
 {
   # System-wide packages
   environment.systemPackages = with pkgs; [
-    # Libre office
-    libreoffice
-    hunspell
-    hunspellDicts.en_US
-    hunspellDicts.ro_RO
     # Gnome extensions
     gnomeExtensions.system-monitor
     gnomeExtensions.alphabetical-app-grid
@@ -16,25 +11,32 @@
     gnomeExtensions.blur-my-shell
     gnomeExtensions.dash-to-dock
     gnomeExtensions.dash-to-panel
-    # Stable packages
+    # Shell    
     curl
     wget
     jq
     file
     traceroute
     tree
-    git
     parted
-    gparted
     f2fs-tools
-    vlc
     mkpasswd
-    trayscale
+    # Utilities
+    git
+    gparted
     ffmpeg-full
     intel-gpu-tools
     libva-utils
     libinput
-    # Unstable packages
+    # Stable apps
+    vlc
+    trayscale
+    # Libre office
+    libreoffice
+    hunspell
+    hunspellDicts.en_US
+    hunspellDicts.ro_RO
+    # Unstable apps
     unstable.microsoft-edge
     unstable.ventoy-full-gtk
     unstable.telegram-desktop
