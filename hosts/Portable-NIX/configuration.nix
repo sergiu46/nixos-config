06c5c374e2effadef7552efefdfc9a4a6c1da875
sergiu46 <38151078+sergiu46@users.noreply.github.com>
2026-02-02 07:53:58 +0200

    disable usb autosuspend

--- hosts/Portable-NIX/configuration.nix
+++ hosts/Portable-NIX/configuration.nix
@@ -25,6 +25,7 @@
       "kvm-intel"
     ];
     kernelParams = [
+      "usbcore.autosuspend=-1"
       "initcall_parallel=1" # Faster boot
       "scsi_mod.use_blk_mq=1" # Multi-queue for storage
       "intel_pstate=active"
