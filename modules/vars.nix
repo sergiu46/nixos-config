c806c833a513f4fbb387d65b70df5eeb53a21f2f
sergiu46 <38151078+sergiu46@users.noreply.github.com>
2026-02-04 05:33:11 +0000

    Update vars.nix

--- modules/vars.nix
+++ modules/vars.nix
@@ -1,45 +1,29 @@
 { configName, lib, ... }:
 
-rec {
-
-  # Btrfs Master Settings (Internal Drive)
+let
+  # Define the lists here so we can use them in the strings below
+  btrfsOpts = [ "noatime" "compress=zstd:1" "ssd" "discard=async" "space_cache=v2" ];
+  
+  f2fsOpts = [
+    "noatime" "lazytime" "compress_algorithm=zstd:1" "compress_chksum"
+    "compress_mode=fs" "compress_extension=*" "atgc" "gc_merge"
+    "flush_merge" "discard" "checkpoint_merge" "active_logs=2"
+    "reserve_root=16384" "inline_xattr" "inline_data" "inline_dentry"
+  ];
+in
+{
   btrfs = {
     label = configName;
-    optsList = [
-      "noatime"
-      "compress=zstd:1"
-      "ssd"
-      "discard=async"
-      "space_cache=v2"
-    ];
-    optsString = builtins.concatStringsSep "," btrfs.optsList;
+    optsList = btrfsOpts;
+    optsString = lib.concatStringsSep "," btrfsOpts;
   };
 
-  # F2FS Master Settings (External/Portable Drive)
   f2fs = {
     label = configName;
-    optsList = [
-      "noatime"
-      "lazytime"
-      "compress_algorithm=zstd:1"
-      "compress_chksum"
-      "compress_mode=fs"
-      "compress_extension=*"
-      "atgc"
-      "gc_merge"
-      "flush_merge"
-      "discard"
-      "checkpoint_merge"
-      "active_logs=2"
-      "reserve_root=16384"
-      "inline_xattr"
-      "inline_data"
-      "inline_dentry"
-    ];
-    optsString = builtins.concatStringsSep "," f2fs.optsList;
+    optsList = f2fsOpts;
+    optsString = lib.concatStringsSep "," f2fsOpts;
   };
 
-  # Portable boot label
-  efiLabel = (lib.toUpper (builtins.substring 0 4 configName)) + "EFI";
-
+  # Porable boot partition label
+  efiLabel = "${lib.toUpper (builtins.substring 0 4 configName)}EFI";
 }
