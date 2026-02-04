60dc84c34e529c5fa57f48ea33bcd45decba77cc
sergiu46 <38151078+sergiu46@users.noreply.github.com>
2026-02-04 05:50:39 +0000

    common module

--- flake.nix
+++ flake.nix
@@ -11,73 +11,34 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nix-flatpak, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       stateVersion = "25.11";
 
-      # Helper function to generate host configurations
       mkHost = configName: modules: 
         let 
-          # Import variables directly into userVars
-          userVars = import ./modules/userVars.nix { 
+          userVars = import ./modules/vars.nix { 
             inherit (nixpkgs) lib; 
             inherit configName; 
           };
         in nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs stateVersion configName userVars; };
-          modules = [
-            # Global Unstable Overlay
-            ({ ... }: {
-              nixpkgs.overlays = [
-                (final: prev: {
-                  unstable = import nixpkgs-unstable {
-                    inherit system;
-                    config = { 
-                      allowUnfree = true; 
-                      allowInsecurePredicate = (pkg: true); 
-                    };
-                  };
-                })
-              ];
-            })
-            nix-flatpak.nixosModules.nix-flatpak
-            home-manager.nixosModules.home-manager
-            {
-              nixpkgs.hostPlatform = system;
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = { inherit stateVersion configName userVars; };
-              };
-            }
-          ] ++ modules;
+          # We pass everything common.nix needs through specialArgs
+          specialArgs = { inherit inputs system stateVersion configName userVars; };
+          modules = [ ./modules/common.nix ] ++ modules;
         };
     in
     {
       nixosConfigurations = {
-
         Latitude-NIX = mkHost "Latitude-NIX" [
           ./hosts/Latitude-NIX/configuration.nix
           ./users/sergiu/sergiu.nix
           ./users/denisa/denisa.nix
         ];
 
-        Samsung-NIX = mkHost "Samsung-NIX" [
-          ./hosts/Portable-NIX/configuration.nix
-          ./users/sergiu/sergiu.nix
-        ];
-
-        Kingston-NIX = mkHost "Kingston-NIX" [
-          ./hosts/Portable-NIX/configuration.nix
-          ./users/sergiu/sergiu.nix
-        ];
-
-        ADATA-NIX = mkHost "ADATA-NIX" [
-          ./hosts/Portable-NIX/configuration.nix
-          ./users/sergiu/sergiu.nix
-        ];
-
+        Samsung-NIX  = mkHost "Samsung-NIX"  [ ./hosts/Portable-NIX/configuration.nix ./users/sergiu/sergiu.nix ];
+        Kingston-NIX = mkHost "Kingston-NIX" [ ./hosts/Portable-NIX/configuration.nix ./users/sergiu/sergiu.nix ];
+        ADATA-NIX    = mkHost "ADATA-NIX"    [ ./hosts/Portable-NIX/configuration.nix ./users/sergiu/sergiu.nix ];
       };
     };
 }
