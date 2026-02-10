893643cba99bb1dbd018eb97a0d23172edfdbf43
Sergiu <sergiu@example.com>
2026-02-10 20:03:02 +0200

    configuration limit

--- modules/system.nix
+++ modules/system.nix
@@ -185,19 +185,17 @@
       "nix-command"
       "flakes"
     ];
-
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
     ];
-
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
 
-  boot.loader.systemd-boot.configurationLimit = 20;
+  boot.loader.systemd-boot.configurationLimit = 30;
   nixpkgs.config.allowUnfree = true;
 
   # System state version
