41175fbcd1b074d90e9b54a1b44dd1a3c784d82d
Sergiu <sergiu@example.com>
2026-02-16 07:52:56 +0200

    remove what is not working

--- modules/tmpfs.nix
+++ modules/tmpfs.nix
@@ -28,14 +28,9 @@
 
       # GNOME SETUP
       mkdir -p /home/sergiu/.cache/gvfs-metadata
-      mkdir -p /home/sergiu/.cache/gnome-bits
       mkdir -p /home/sergiu/.local/share/
       rm -rf /home/sergiu/.local/share/gvfs-metadata
       ln -sfn /home/sergiu/.cache/gvfs-metadata /home/sergiu/.local/share/gvfs-metadata
-      rm -rf /home/sergiu/.local/share/recently-used.xbel
-      touch /home/sergiu/.cache/gnome-bits/recently-used.xbel
-      ln -sfn /home/sergiu/.cache/gnome-bits/recently-used.xbel /home/sergiu/.local/share/recently-used.xbel
-
     '';
   };
 
