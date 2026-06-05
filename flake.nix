017bff091a8fc4b89b57370e083f2f929c78a528
Sergiu <sergiu@example.com>
2026-06-05 17:58:42 +0300

    Upgrade to 26.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "My NixOS systems with flakes, Home Manager, and declarative Flatpaks";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
