2082cef1f9e2bf525e924f94cf9adb4a5294d41b
sergiu46 <38151078+sergiu46@users.noreply.github.com>
2026-06-09 08:06:47 +0300

    Change nixpkgs to flakehub

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "My NixOS systems with flakes, Home Manager, and declarative Flatpaks";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "https://flakehub.com/f/nix-community/home-manager/*";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
