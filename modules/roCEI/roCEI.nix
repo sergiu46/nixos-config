1945edb9e365e0864f49a871a909e925f1a10d6c
Sergiu <sergiu@example.com>
2026-07-18 14:40:52 +0300

    .

--- modules/roCEI/roCEI.nix
+++ modules/roCEI/roCEI.nix
@@ -11,7 +11,7 @@ let
     nativeBuildInputs = with pkgs; [
       dpkg
       autoPatchelfHook
-      makeWrapper
+      wrapGAppsHook3 # MODIFIED
     ];
 
     buildInputs = with pkgs; [
@@ -24,19 +24,21 @@ let
       cairo
       pango
       gdk-pixbuf
-      libx11 # MODIFICAT
-      openssl # ADAUGAT
-      libjpeg8 # ADAUGAT
+      libx11
+      openssl
+      libjpeg8
     ];
 
     dontConfigure = true;
     dontBuild = true;
 
-    unpackPhase = ''
-      runHook preUnpack
-      dpkg-deb -x $src .
-      runHook postUnpack
-    '';
+    # MODIFIED: Arguments passed automatically to the GTK wrapper
+    gappsWrapperArgs = [
+      "--prefix"
+      "LD_LIBRARY_PATH"
+      ":"
+      "${pkgs.pcsclite}/lib"
+    ];
 
     installPhase = ''
       runHook preInstall
@@ -47,8 +49,8 @@ let
 
       if [ -f "$out/bin/idplugclassic/identitymanager" ]; then
         mkdir -p $out/bin
-        makeWrapper "$out/bin/idplugclassic/identitymanager" "$out/bin/idplugclassic-manager" \
-          --prefix LD_LIBRARY_PATH : "${pkgs.pcsclite}/lib"
+        # MODIFIED: Symlink created; wrapGAppsHook3 will wrap the target binary automatically
+        ln -s "$out/bin/idplugclassic/identitymanager" "$out/bin/idplugclassic-manager"
       fi
       runHook postInstall
     '';
@@ -66,4 +68,9 @@ in
 
   # Install the package system-wide
   environment.systemPackages = [ ro-cei-pkg ];
+
+  # Creates a symlink in /usr/share to fix hardcoded asset paths
+  systemd.tmpfiles.rules = [
+    "L+ /usr/share/idplugclassic - - - - ${ro-cei-pkg}/share/idplugclassic"
+  ];
 }
