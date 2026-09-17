d67431385880bc07b7aa50c363c62313826f7846
Sergiu <sergiu@example.com>
2026-09-17 10:55:58 +0300

    cleanup

--- hosts/Portable-NIX/configuration.nix
+++ hosts/Portable-NIX/configuration.nix
@@ -182,23 +182,6 @@
       "modesetting" # Intel & Generic fallback
     ];
 
-    # udev.extraRules = ''
-    #   # BFQ for internal NVMe/SSD
-    #   ACTION=="add|change", \
-    #     KERNEL=="nvme[0-9]*|mmcblk[0-9]*", \
-    #     ATTR{queue/rotational}=="0", \
-    #     ATTR{queue/scheduler}="bfq"
-
-    #   # mq-deadline for removable USBs
-    #   ACTION=="add|change", \
-    #     KERNEL=="sd[a-z]*", \
-    #     ATTR{removable}=="1", \
-    #     ATTR{queue/scheduler}="mq-deadline"
-
-    #   # Ghost Mode: Hide internal drives of the host machine
-    #   # SUBSYSTEM=="block", ATTRS{removable}=="0", ENV{UDISKS_IGNORE}="1"
-    # '';
-
   };
 
   # Extra firmware packages
